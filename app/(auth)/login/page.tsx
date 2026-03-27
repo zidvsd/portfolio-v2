3f5f1743d8fc04013afa6b1d9fce01a632232fa1
Zid <rashidvisda@gmail.com>
2026-03-27 20:33:10 +0800

    fix secret

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,11 @@
-"use client"
-
 import { LoginForm } from "@/components/login-form"
-import { RowsIcon } from "@phosphor-icons/react"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col">
+    <div className="flex min-h-svh flex-col bg-background">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-lg">
             <LoginForm />
           </div>
         </div>
